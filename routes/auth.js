d208e8f195b0920c5973bd53f504d683d2a3ced4
Yassin6687 <yassin2406210@miuegypt.edu.eg>
2026-06-07 03:17:13 +0300

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
-const User = require('../models/User')
+const User = require('../models/user')
 
 router.post('/register', async (req, res) => {
   try {
