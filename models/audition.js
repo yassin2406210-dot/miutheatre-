8603c3e61b9e573a3755c230bf914cdf971536ce
jana2403303-ux <jana2403303@miuegypt.edu.eg>
2026-06-07 21:53:01 +0300

    update

--- models/audition.js
+++ models/audition.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const auditionSchema = new mongoose.Schema({
   name:       { type: String, required: true, trim: true },
   email:      { type: String, required: true, lowercase: true, trim: true },
-  phone:      { type: String, trim: true },
+  student_id: { type: String, trim: true },
   year:       { type: String },
   major:      { type: String },
   experience: { type: String },
