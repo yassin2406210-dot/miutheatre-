dd85ded947aac4189cb3a35529a20bb64bf39885
mary2407700-cmd <mary2407700@miuegypt.edu.eg>
2026-06-07 21:22:01 +0200

    Update contact.js

--- models/contact.js
+++ models/contact.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const contactSchema = new mongoose.Schema({
   firstName: { type: String, required: true, trim: true },
-  lastName:  { type: String, trim: true },
+  lastName:  { type: String, required: true, trim: true },
   email:     { type: String, required: true, lowercase: true, trim: true },
   subject:   { type: String },
   message:   { type: String, required: true }
